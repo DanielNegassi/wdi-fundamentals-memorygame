d772e901b5258ecde20ae1615218a4a2890995ab
Daniel Negassi <dyn29@hotmail.com>
2018-06-10 23:23:39 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,12 @@
-var cardOne = "queen"
-var cardTwo = "king"
-var cardThree = "queen"
-var cardFour = "king"
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = []; 
+var cardOne = cards[0];
+cardsInPlay.push( "cardOne");
+console.log("User flipped queen");
+var cardTwo = cards[2];
+cardsInPlay.push( "cardTwo");
+console.log("User flipped king");
+if ( cardsInPlay.length === 2 && cardOne === cardTwo )
+	{ alert("Match found!"); } 
+else if(cardsInPlay.length === 2 && cardOne !== cardTwo) 
+	{ alert("Sorry, try again!"); } 
